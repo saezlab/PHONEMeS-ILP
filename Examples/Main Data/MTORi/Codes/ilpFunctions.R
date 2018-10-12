d7585bfbfb23fc3092d4d6609945dfa79794696d
enio23 <enio.gjerga@gmail.com>
2018-10-12 16:15:31 +0200

    Update ilpFunctions.R

--- Examples/Main Data/MTORi/Codes/ilpFunctions.R
+++ Examples/Main Data/MTORi/Codes/ilpFunctions.R
@@ -537,84 +537,6 @@ readOutSIF<- function(cplexSolutionFileName, binaries = binaries){
 }
 
 ##
-readOutSIFAll<- function(cplexSolutionFileName, binaries = binaries){
-  
-  reacIDX <- c()
-  for(i in 1:length(binaries[[3]])){
-    if(strsplit(binaries[[3]][i], split = " ")[[1]][1] == "reaction"){
-      reacIDX <- c(reacIDX, i)
-    }
-  }
-  reacVar <- binaries[[1]][reacIDX]
-  
-  cplexSolutionData <- xmlParse(cplexSolutionFileName)
-  cplexSolution <- xmlToList(cplexSolutionData)
-  
-  cplexSolutionEdgesAll <- list()
-  sifAll <- list()
-  
-  for(ii in 1:(length(cplexSolution)-1)){
-    
-    sif <- matrix(data = NA, nrow = 1, ncol = 3)
-    
-    currSolution <- cplexSolution[[ii]][[4]]
-    
-    for(jj in 1:length(currSolution)){
-      
-      if((currSolution[[jj]][1]%in%reacVar) && round(as.numeric(currSolution[[jj]][3])==1)){
-        
-        reaction <- strsplit(binaries[[3]][which(binaries[[1]]==currSolution[[jj]][1])], split = " ")[[1]][2]
-        
-        sif2bind <- as.matrix(c(strsplit(x = reaction, split = "=")[[1]][1], "1", strsplit(x = reaction, split = "=")[[1]][2]))
-        sif2bind <- t(sif2bind)
-        sif <- rbind(sif, sif2bind)
-        
-      }
-      
-    }
-    
-    sifAll[[length(sifAll)+1]] <- unique(sif[-1, ])
-    
-  }
-  
-  for(ii in 1:length(sifAll)){
-    
-    if(ii==1){
-      
-      sif <- sifAll[[1]]
-      
-    } else {
-      
-      # sif <- unique(rbind(sif, sifAll[[ii]]))
-      for(jj in 1:nrow(sifAll[[ii]])){
-        
-        idx1 <- which(sif[, 1]==sifAll[[ii]][jj, 1])
-        idx2 <- which(sif[, 3]==sifAll[[ii]][jj, 3])
-        
-        idx <- intersect(x = idx1, y = idx2)
-        
-        if(length(idx) > 0){
-          
-          sif[idx, 2] <- as.character(as.numeric(sif[idx, 2])+1)
-          
-        } else {
-          
-          sif <- rbind(sif, sifAll[[ii]][jj, ])
-          
-        }
-        
-      }
-      
-    }
-    
-  }
-  
-  return(sif)
-  
-}
-
-
-##
 reducedModel <- function(sif = sif, dataMatrix = dataMatrix){
   
   gg <- igraph.from.graphNEL(sif2graph(sif = sif))
@@ -925,5 +847,82 @@ write_self_activating_constraints <- function(pknList = pknList, binaries = bina
   cc3 <- paste0(distVar, " <= ", M)
   
   return(c(cc1, cc2, cc3))
+  
+}
+
+##
+readOutSIFAll<- function(cplexSolutionFileName, binaries = binaries){
+  
+  reacIDX <- c()
+  for(i in 1:length(binaries[[3]])){
+    if(strsplit(binaries[[3]][i], split = " ")[[1]][1] == "reaction"){
+      reacIDX <- c(reacIDX, i)
+    }
+  }
+  reacVar <- binaries[[1]][reacIDX]
+  
+  cplexSolutionData <- xmlParse(cplexSolutionFileName)
+  cplexSolution <- xmlToList(cplexSolutionData)
+  
+  cplexSolutionEdgesAll <- list()
+  sifAll <- list()
+  
+  for(ii in 1:(length(cplexSolution)-1)){
+    
+    sif <- matrix(data = NA, nrow = 1, ncol = 3)
+    
+    currSolution <- cplexSolution[[ii]][[4]]
+    
+    for(jj in 1:length(currSolution)){
+      
+      if((currSolution[[jj]][1]%in%reacVar) && round(as.numeric(currSolution[[jj]][3])==1)){
+        
+        reaction <- strsplit(binaries[[3]][which(binaries[[1]]==currSolution[[jj]][1])], split = " ")[[1]][2]
+        
+        sif2bind <- as.matrix(c(strsplit(x = reaction, split = "=")[[1]][1], "1", strsplit(x = reaction, split = "=")[[1]][2]))
+        sif2bind <- t(sif2bind)
+        sif <- rbind(sif, sif2bind)
+        
+      }
+      
+    }
+    
+    sifAll[[length(sifAll)+1]] <- unique(sif[-1, ])
+    
+  }
+  
+  for(ii in 1:length(sifAll)){
+    
+    if(ii==1){
+      
+      sif <- sifAll[[1]]
+      
+    } else {
+      
+      # sif <- unique(rbind(sif, sifAll[[ii]]))
+      for(jj in 1:nrow(sifAll[[ii]])){
+        
+        idx1 <- which(sif[, 1]==sifAll[[ii]][jj, 1])
+        idx2 <- which(sif[, 3]==sifAll[[ii]][jj, 3])
+        
+        idx <- intersect(x = idx1, y = idx2)
+        
+        if(length(idx) > 0){
+          
+          sif[idx, 2] <- as.character(as.numeric(sif[idx, 2])+1)
+          
+        } else {
+          
+          sif <- rbind(sif, sifAll[[ii]][jj, ])
+          
+        }
+        
+      }
+      
+    }
+    
+  }
+  
+  return(sif)
   
 }
