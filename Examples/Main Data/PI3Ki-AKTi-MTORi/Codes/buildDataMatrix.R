b560515b9db8b5aa2edbc7785eda2639b4c3fe24
enio23 <enio.gjerga@gmail.com>
2018-10-30 16:54:56 +0100

    Update function

--- Examples/Main Data/PI3Ki-AKTi-MTORi/Codes/buildDataMatrix.R
+++ Examples/Main Data/PI3Ki-AKTi-MTORi/Codes/buildDataMatrix.R
@@ -18,122 +18,56 @@
 
 buildDataMatrix <- function(dataGMM = dataGMM, pknList = pknList, targets, experiments){
   
-  tg <- unlist(targets)
+  TG <- unlist(unique(targets))
+  sites <- intersect(pknList@species, dataGMM@IDmap$S.cc)
+  idxTG <- list()
+  idxDN <- list()
+  idxDS <- list()
   
-  idxTG <- which(pknList@species%in%tg)
-  idxDS <- which(!is.na(match(pknList@species, intersect(dataGMM@IDmap$S.cc, pknList@species))))
-  idxDN <- setdiff(1:length(pknList@species), c(idxTG, idxDS))
-  
-  cNames <- c()
-  for(i in 1:length(pknList@species)){
-    
-    if(i %in% idxTG){
-      
-      cNames <- c(cNames, paste0("TG:", pknList@species[i]))
-      
-    }
-    
-    if(i %in% idxDN){
-      
-      cNames <- c(cNames, paste0("DN:", pknList@species[i]))
-      
-    }
-    
-    if(i %in% idxDS){
-      
-      cNames <- c(cNames, paste0("DS:", pknList@species[i]))
-      
-    }
-    
+  dataMatrix <- matrix(data = 0, nrow = length(experiments), ncol = length(pknList@species))
+  cNames <- unique(c(TG, setdiff(pknList@species, c(TG, sites)), sites))
+  idx <- which(cNames%in%sites)
+  for(ii in 1:length(experiments)){
+    idxDS[[length(idxDS)+1]] <- idx
   }
+  cNames[setdiff(1:length(cNames), idx)] <- paste0("DN:", cNames[setdiff(1:length(cNames), idx)])
+  cNames[idx] <- paste0("DS:", cNames[idx])
   
-  dataMatrix <- matrix(, nrow = length(experiments), ncol = length(pknList@species))
   colnames(dataMatrix) <- cNames
-  dataMatrix[, c(idxTG, idxDN)] <- 0
   
-  for(i in 1:length(experiments)){
+  for(ii in 1:nrow(dataMatrix)){
     
-    dM <- matrix(, nrow = length(experiments[[i]]), ncol = length(idxDS))
-    colnames(dM) <- cNames[idxDS]
-    rownames(dM) <- experiments[[i]]
-    
-    for(ii in 1:nrow(dM)){
+    cc <- conditions[[ii]]
       
-      for(jj in 1:ncol(dM)){
+      for(kk in 1:length(sites)){
         
-        site <- strsplit(colnames(dM)[jj], split = ":")[[1]][2]
-        siteID <- dataGMM@IDmap$dataID[which(dataGMM@IDmap$S.cc==site)][1]
-        id <- which(names(dataGMM@res)==siteID)[1]
+        nn <- dataGMM@IDmap$dataID[which(dataGMM@IDmap$S.cc==sites[kk])]
+        idxMeas <- which(names(dataGMM@res)==nn)
         
-        if(!is.na(id)){
+        if(length(idxMeas) > 0){
           
-          if(!is.na(dataGMM@res[[id]][rownames(dM)[ii], 4])){
-            
-            if(dataGMM@res[[id]][rownames(dM)[ii], 4]=="OK"){
-              
-              dM[ii, jj] <- as.numeric(dataGMM@res[[id]][rownames(dM)[ii], 1])
-              
-            }
-            
-          }
+          score <- min(as.numeric(dataGMM@res[idxMeas][[1]][which(rownames(dataGMM@res[idxMeas][[1]])%in%cc), 1]))
+          
+          dataMatrix[ii, which(cNames==paste0("DS:", sites[kk]))] <- score
           
         }
         
       }
-      
-    }
-    
-    dM[which(as.character(dM)=="-Inf")] <- -9999
-    dM[which(as.character(dM)=="Inf")] <- 9999
-    
-    for(j in 1:length(idxDS)){
-      
-      if(!all(is.na(dM[, j]))){
-        
-        dataMatrix[i, idxDS] <- colSums(dM, na.rm = TRUE)
-        
-      }
-      
-    }
     
   }
   
-  dataMatrix[which(as.character(dataMatrix)=="-Inf")] <- -9999
-  dataMatrix[which(as.character(dataMatrix)=="Inf")] <- 9999
-  naIdx <- c()
-  for(i in 1:length(idxDS)){
+  if(length(which(is.na(dataMatrix)))>0){dataMatrix[which(is.na(dataMatrix))] <- 0}
+  if(length(intersect(which(dataMatrix < 0), which(is.infinite(dataMatrix))))>0){dataMatrix[intersect(which(dataMatrix < 0), which(is.infinite(dataMatrix)))] <- -9999}
+  if(length(dataMatrix[intersect(which(dataMatrix > 0), which(is.infinite(dataMatrix)))] <- 9999)>0){dataMatrix[intersect(which(dataMatrix > 0), which(is.infinite(dataMatrix)))] <- 9999}
+  
+  for(ii in 1:length(targets)){
     
-    if(all(is.na(dataMatrix[, idxDS[i]]))){
-      
-      naIdx <- c(naIdx, idxDS[i])
-      
-    }
+    idxTG[[length(idxTG)+1]] <- which(cNames%in%paste0("DN:", targets[[ii]]))
+    idxDN[[length(idxDN)+1]] <- setdiff(1:length(cNames), unique(c(idxDS[[ii]], idxTG[[ii]])))
     
   }
   
-  if(length(naIdx) > 0){
-    
-    for(kk in 1:length(naIdx)){
-      
-      idxDN <- c(idxDN, naIdx[kk])
-      idxDS <- idxDS[-which(idxDS==naIdx[kk])]
-      
-      cNames[naIdx[kk]] <- paste0("DN:", strsplit(cNames[naIdx[kk]], split = ":")[[1]][2])
-      
-    }
-    
-  }
-  colnames(dataMatrix) <- cNames
-  dataMatrix[is.na(dataMatrix)] <- 0
-  
-  res <- apply(dataMatrix, 2, min)
-  
-  res <- t(as.matrix(res))
-  dataMatrix <- res
-  
-  dataMatrix[1, which(dataMatrix[1, ] > 0)] <- 100*dataMatrix[1, which(dataMatrix[1, ] > 0)]
-  
-  res <- list(dataMatrix=dataMatrix, tgID=idxTG, dnID=idxDN, dsID=idxDS, species=pknList@species)
+  res <- list(dataMatrix=dataMatrix, tgID=idxTG, dnID=idxDN, dsID=idxDS, species=unique(c(TG, setdiff(pknList@species, c(TG, sites)), sites)))
   
   return(res)
   
